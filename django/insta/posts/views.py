aedb0df4b16c8aba839b44bba140e0cf3c7c7349
Minjae Jin <moc0etan@gmail.com>
2019-04-15 08:49:20 +0000

    190415

--- django/insta/posts/views.py
+++ django/insta/posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST, require_http_methods
-from .forms import PostForm, CommentForm
+from .forms import PostForm, CommentForm, ImageFormSet
 from .models import Post, Comment
+from django.db import transaction
 
 def list(request):
     posts = Post.objects.order_by('-id').all()
@@ -13,15 +14,27 @@ def list(request):
 @login_required
 def create(request):
     if request.method == 'POST':
-        post_form = PostForm(request.POST, request.FILES)
-        if post_form.is_valid():
+        post_form = PostForm(request.POST)
+        image_formset = ImageFormSet(request.POST, request.FILES)
+        if post_form.is_valid() and image_formset.is_valid():
             post = post_form.save(commit=False)
             post.user = request.user
-            post.save() # 실제 데이터베이스에 저장
+            
+            with transaction.atomic():
+                # 첫번째
+                post.save() # 실제 데이터베이스에 저장
+                # 두번째
+                image_formset.instance = post
+                image_formset.save() # 실제 데이터베이스에 저장
+            
             return redirect('posts:list')
     else:
         post_form = PostForm()
-    return render(request, 'posts/form.html', {'post_form':post_form})
+        image_formset = ImageFormSet()
+    return render(request, 'posts/form.html', {
+        'post_form':post_form, 
+        'image_formset':image_formset
+    })
     
 
 @login_required
@@ -31,12 +44,18 @@ def update(request, post_id):
         return redirect('posts:list')
     if request.method == 'POST':
         post_form = PostForm(request.POST, request.FILES, instance=post)
-        if post_form.is_valid():
+        image_formset = ImageFormSet(request.POST, request.FILES, instance=post)
+        if post_form.is_valid() and image_formset.is_valid():
             post_form.save()
+            image_formset.save()
             return redirect('posts:list')
     else:
         post_form = PostForm(instance=post)
-    return render(request, 'posts/form.html', {'post_form':post_form})
+        image_formset = ImageFormSet(instance=post)
+    return render(request, 'posts/form.html', {
+        'post_form':post_form, 
+        'image_formset':image_formset
+    })
 
 
 @login_required
